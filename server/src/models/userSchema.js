931a5c4fe5a3c378eda3ccba3c60f5fb1a740aee
Justin Hamerly <suvimountain@icloud.com>
2022-09-08 15:15:14 -0700

    register code changes.  no longer keeping password in state

--- server/src/models/userSchema.js
+++ server/src/models/userSchema.js
@@ -9,12 +9,9 @@ const userSchema = new Schema(
     username: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
-      type: String,
-      required: true
-    },
-    email: {
       type: String,
       required: true
     }
